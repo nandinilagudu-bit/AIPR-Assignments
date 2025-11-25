73b3e80de597346799350a6a898826e5e04cb614
nandinilagudu-bit <nandinilagudu77@gmail.com>
2025-11-25 21:52:10 +0530

    Delete task2 .py

--- task2 .py
+++
@@ -1,9 +0,0 @@
-def read_file(filename):
-    try:
-        with open(filename, "r") as f:
-            data = f.read()
-        return data
-    except FileNotFoundError:
-        return "Error: File not found."
-    except IOError:
-        return "Error: Unable to read file."
